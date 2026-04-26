63fc27d4a1e15d5fed5fc0410606bf27de6f3e77
rajath hegde <138708414+rajath-hk@users.noreply.github.com>
2026-04-26 21:37:53 +0530

    fix1

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -65,6 +65,7 @@ const Portfolio = () => {
     const portfolioItems = {
         fastPaced: [1, 2, 3, 4],
         motionGraphics: [1, 2, 3, 4],
+        aiReels: [1, 2, 3, 4],
         youtube: [
             { title: 'Documentary Edit', type: 'doc' },
             { title: 'Music Video Edit', type: 'music' }
