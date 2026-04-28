31248efa817f4df39f239b979b46d2188c6fdfe3
Priyansxyz <guddudandapath97@gmail.com>
2026-04-28 19:43:33 +0530

    Update videos 2

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -101,10 +101,10 @@ const Portfolio = () => {
             { title: 'Fast Paced Edit 4', type: 'short', videoId: 'dHJg6uceDyw' }
         ],
         motionGraphics: [
-            { title: 'Motion Graphics Short 1', type: 'short', videoId: 'ORbp7xWkFtY' },
+            { title: 'Motion Graphics Short 1', type: 'short', videoId: '8SB9eBiQ5rhZGBaD' },
             { title: 'Motion Graphics Short 2', type: 'short', videoId: '9VNqKz8nhq4' },
             { title: 'Motion Graphics Short 3', type: 'short', videoId: '2VFJNTEjmTQ' },
-            { title: 'Motion Graphics Short 3', type: 'short', videoId: '2VFJNTEjmTQ' }
+            { title: 'Motion Graphics Short 3', type: 'short', videoId: 'ORbp7xWkFtY' }
         ],
         youtube: [
             { title: 'Long form', type: 'doc', videoId: 'jFvz7VQbN2g' },
